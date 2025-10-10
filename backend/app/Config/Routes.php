67555db8edebec1a93fc91b973887c320c94eba6
callckhub100 <callkess01@gmail.com>
2025-10-11 01:28:00 +0800

    refactor(route): update the route for landing page

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Users::index');
+$routes->get('/', 'Users::landing');
 $routes->get('/moodboard', 'Users::moodboard');
 $routes->get('/roadmap', 'Users::roadmap');
 $routes->get('/login', 'Users::login');
